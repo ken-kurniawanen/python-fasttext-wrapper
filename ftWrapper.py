68313cb4999109f363400eaf0fd38929c795cb02
Kurniawan Eka Nugraa <kurniawanekn@gmail.com>
2019-05-03 20:15:33 +0700

    Add cross_val_score,simulation,grid_search

--- ftWrapper.py
+++ ftWrapper.py
@@ -16,12 +16,16 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+import warnings
+warnings.filterwarnings('ignore') 
 
 # specify fastText directory
 Path = '/jet/prs/workspace/fastText-0.2.0/'
 
 
-def train(X,y, pretrained = None, dim=100 ,Ngrams=1,epochs=5, minn=3,maxn=6, model="model"):
+def train(X,y, pretrained = None, dim=300 ,Ngrams=1,epochs=5, minn=3,maxn=6, model="model"):
     """
     Equivalent to ./fasttext supervised
     
@@ -56,8 +60,8 @@ def train(X,y, pretrained = None, dim=100 ,Ngrams=1,epochs=5, minn=3,maxn=6, mod
     # error checking, pass STDERR or STDOUT to Python
     try:
         out = subprocess.run(cmd, stderr=subprocess.PIPE, shell=True, check=True)
-        print(cmd)
-        print(out.stderr.decode('utf-8'))
+        #print(cmd)
+        #print(out.stderr.decode('utf-8'))
     except Exception:
         result = subprocess.run(cmd, stderr=subprocess.PIPE, shell=True)
         print(cmd)
@@ -137,7 +141,7 @@ def predict(X, model="model.bin"):
 
     return outputPredictions
 
-def cross_val_score(data, pretrained = None, dim=100 ,Ngrams=1,epochs=5, minn=3,maxn=6,k=5):
+def cross_val_score(data, pretrained = None, dim=300 ,Ngrams=1,epochs=5, minn=3,maxn=6,k=5):
     """
     Run cross validation based on fastText model and sklearn style data.
     Return list of evaluation metric.
@@ -161,21 +165,65 @@ def cross_val_score(data, pretrained = None, dim=100 ,Ngrams=1,epochs=5, minn=3,
     
     for fold in range(1,k+1):
         
-        test = data.loc[len(data)//k*(fold-1):len(data)//k*fold-1]
-        train = data[~data.index.isin(test.index)]
+        test_ = data.loc[len(data)//k*(fold-1):len(data)//k*fold-1]
+        train_ = data[~data.index.isin(test_.index)]
         
-        train(train['text'].tolist(),train['label'].tolist(), dim=dim, pretrained=pretrained, minn=minn ,maxn=maxn, epochs = epochs)
-        pred = predict(test['text'].tolist())
+        train(train_['text'].tolist(),train_['label'].tolist(), dim=dim, pretrained=pretrained, minn=minn ,maxn=maxn, epochs = epochs)
+        pred = predict(test_['text'].tolist())
 
-        acc_list.append(accuracy_score(pred, test['label'].tolist()))
-        precision_list.append(precision_score(pred, test['label'].tolist(), average='weighted'))
-        f1_list.append(f1_score(pred, test['label'].tolist(), average='weighted'))
-        conf_matrix = confusion_matrix(pred, test['label'].tolist())
+        acc_list.append(accuracy_score(pred, test_['label'].tolist()))
+        precision_list.append(precision_score(pred, test_['label'].tolist(), average='weighted'))
+        f1_list.append(f1_score(pred, test_['label'].tolist(), average='weighted'))
+        conf_matrix = confusion_matrix(pred, test_['label'].tolist())
 
-    print("Accuracy : %0.3f (+/- %0.2f)" % (np.mean(acc_list), np.std(acc_list) * 2))
-    print("Precision : %0.3f (+/- %0.2f)" % (np.mean(precision_list), np.std(precision_list) * 2))
-    print("F1-score : %0.3f (+/- %0.2f)" % (np.mean(f1_list), np.std(f1_list) * 2))
-    print(conf_matrix)
+    # print("Accuracy : %0.3f (+/- %0.2f)" % (np.mean(acc_list), np.std(acc_list) * 2))
+    # print("Precision : %0.3f (+/- %0.2f)" % (np.mean(precision_list), np.std(precision_list) * 2))
+    # print("F1-score : %0.3f (+/- %0.2f)" % (np.mean(f1_list), np.std(f1_list) * 2))
+    # print(conf_matrix)
     
     return acc_list,precision_list,f1_list
 
+def simulation(data, pretrained = None, dim=300 ,Ngrams=1,epochs=5, minn=3,maxn=6, metric=["accuracy","precision","f1"]):
+    
+    train_size_list = np.linspace(0.9,0.1,9)
+
+    text = list(data['text'])
+    target = list(data['label'])
+
+    for idx, train_size in enumerate(train_size_list):
+        print("train size : ", train_size, end=' ')
+
+        text_train,text_validation,target_train,target_validation = train_test_split(text,target, test_size=1-train_size, random_state=1)
+        
+        train(X=text_train, y=target_train, dim=dim, pretrained=pretrained, minn=minn, maxn=maxn, epochs=epochs)
+
+        if "accuracy" in metric:
+            print('accuracy : %0.3f'%accuracy_score(predict(text_validation), target_validation),end=' ')
+        if "precision" in metric:
+            print('precision : %0.3f'%precision_score(predict(text_validation),target_validation, average="weighted"),end=' ')
+        if "f1" in metric:
+            print('f1 score : %0.3f'%f1_score(predict(text_validation),target_validation, average="weighted"))
+        if "confusion_matrix" in metric:
+            print(confusion_matrix(predict(text_validation),target_validation))
+
+    return 
+
+def grid_search(data,pretrains=[None,"wiki.id.vec"], epochs=[5,15,50], subwords=["no","yes"]):
+    print('[pretrain, epoch, subword]-> accuracy = x, f1 = x')
+
+    for pretrain in pretrains:
+        for epoch in epochs:
+            for subword in subwords:
+                if 'yes' in subwords:
+                    minn = 3
+                    maxn = 6
+                else:
+                    minn = 0
+                    maxn = 0
+
+                acc_list,_,f1_list = cross_val_score(data,pretrain,dim=300,Ngrams=1,epochs=epoch,minn=minn,maxn=maxn,k=5)
+                print('[',str(pretrain),',',str(epoch),',',str(subword),']', end=' ')
+                print('a : %0.3f (+/- %0.2f)' % (np.mean(acc_list), np.std(acc_list) * 2), end=' ')
+                print('f1 : %0.3f (+/- %0.2f)' % (np.mean(f1_list), np.std(f1_list) * 2))
+
+    return
